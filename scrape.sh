ec7d2ff2bf551d6220cdbd01c87b846ca561b7ef
cookiengineer <cookiengineer@protonmail.ch>
2023-06-10 23:37:06 +0200

    :bug: Better Subreddit list

--- scrape.sh
+++ scrape.sh
@@ -3,6 +3,9 @@
 go build -o ./reddit-archivar ./cmds/reddit-archivar/main.go;
 
 ./reddit-archivar /r/Malware;
+./reddit-archivar /r/MalwareAnalysis;
+./reddit-archivar /r/MalwareDevelopment;
+./reddit-archivar /r/MalwareResearch;
 ./reddit-archivar /r/LinuxMalware;
 ./reddit-archivar /r/ReverseEngineering;
 ./reddit-archivar /r/cybersecurity;
@@ -13,4 +16,11 @@ go build -o ./reddit-archivar ./cmds/reddit-archivar/main.go;
 ./reddit-archivar /r/xss;
 ./reddit-archivar /r/blackhat;
 ./reddit-archivar /r/computerforensics;
+./reddit-archivar /r/redteam;
+./reddit-archivar /r/ExploitDev;
+./reddit-archivar /r/ComputerSecurity;
+./reddit-archivar /r/websec;
+./reddit-archivar /r/websecurity;
+./reddit-archivar /r/owasp;
+./reddit-archivar /r/sysadmin;
 
